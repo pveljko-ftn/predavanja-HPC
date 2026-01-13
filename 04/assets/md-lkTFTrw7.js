f6567f1695804720a3cdb24257753f6629d9e903
Veljko Petrović <pveljko.ftn@gmail.com>
2026-01-13 12:01:08 +0100

    08

---
+++ 04/assets/md-lkTFTrw7.js
@@ -0,0 +1,14 @@
+import{_ as t}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DSP9nhwX.js";import{b as i,o as T,w as a,g as s,e as p,m as R,ad as n,v as o,x as r,T as l}from"./modules/vue-B--U7SYg.js";import{I as M}from"./slidev/default-DbSP6hwu.js";import{u as c,f as u}from"./slidev/context-QbrBJwKo.js";import"./modules/unplugin-icons-Dafapu7P.js";import"./index-DYvNz-nI.js";import"./modules/shiki-0kOzWxKF.js";const g={__name:"04.md__slidev_45",setup(N){const{$clicksContext:S,$frontmatter:e}=c();return S.setup(),(m,I)=>{const G=t;return T(),i(M,o(r(l(u)(l(e),44))),{default:a(()=>[I[1]||(I[1]=s("h2",null,"Signali",-1)),p(G,R({},{title:"",ranges:[]}),{default:a(()=>[...I[0]||(I[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"[veljko@ftn ~]$ kill -l")]),n(`
+`),s("span",{class:"line"},[s("span",null," 1) SIGHUP       2) SIGINT       3) SIGQUIT      4) SIGILL       5) SIGTRAP")]),n(`
+`),s("span",{class:"line"},[s("span",null," 6) SIGABRT      7) SIGBUS       8) SIGFPE       9) SIGKILL     10) SIGUSR1")]),n(`
+`),s("span",{class:"line"},[s("span",null,"11) SIGSEGV     12) SIGUSR2     13) SIGPIPE     14) SIGALRM     15) SIGTERM")]),n(`
+`),s("span",{class:"line"},[s("span",null,"16) SIGSTKFLT   17) SIGCHLD     18) SIGCONT     19) SIGSTOP     20) SIGTSTP")]),n(`
+`),s("span",{class:"line"},[s("span",null,"21) SIGTTIN     22) SIGTTOU     23) SIGURG      24) SIGXCPU     25) SIGXFSZ")]),n(`
+`),s("span",{class:"line"},[s("span",null,"26) SIGVTALRM   27) SIGPROF     28) SIGWINCH    29) SIGIO       30) SIGPWR")]),n(`
+`),s("span",{class:"line"},[s("span",null,"31) SIGSYS      34) SIGRTMIN    35) SIGRTMIN+1  36) SIGRTMIN+2  37) SIGRTMIN+3")]),n(`
+`),s("span",{class:"line"},[s("span",null,"38) SIGRTMIN+4  39) SIGRTMIN+5  40) SIGRTMIN+6  41) SIGRTMIN+7  42) SIGRTMIN+8")]),n(`
+`),s("span",{class:"line"},[s("span",null,"43) SIGRTMIN+9  44) SIGRTMIN+10 45) SIGRTMIN+11 46) SIGRTMIN+12 47) SIGRTMIN+13")]),n(`
+`),s("span",{class:"line"},[s("span",null,"48) SIGRTMIN+14 49) SIGRTMIN+15 50) SIGRTMAX-14 51) SIGRTMAX-13 52) SIGRTMAX-12")]),n(`
+`),s("span",{class:"line"},[s("span",null,"53) SIGRTMAX-11 54) SIGRTMAX-10 55) SIGRTMAX-9  56) SIGRTMAX-8  57) SIGRTMAX-7")]),n(`
+`),s("span",{class:"line"},[s("span",null,"58) SIGRTMAX-6  59) SIGRTMAX-5  60) SIGRTMAX-4  61) SIGRTMAX-3  62) SIGRTMAX-2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"63) SIGRTMAX-1  64) SIGRTMAX")])])],-1)])]),_:1},16)]),_:1},16)}}};export{g as default};
